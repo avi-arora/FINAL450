ecb116d15a4589056d8fa8576ddfb2fadf1e06fe
avishek arora <avishek.arora@icloud.com>
2020-12-01 18:22:58 +0530

    added few questions

---
+++ array/8-kthminmax.py
@@ -0,0 +1,17 @@
+
+def kth_minmax_usingsorting(list):
+    """
+
+    """
+    pass
+
+def kth_minmax(list):
+    """
+    """
+    pass
+
+def kth_usingquickselect(list):
+    """
+    """
+    pass
+
