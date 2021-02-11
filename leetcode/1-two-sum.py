59cf84152575d269ab0bab2c65bf4eeab00dc06f
avishek arora <avishek.arora@icloud.com>
2021-02-11 07:55:11 +0530

    leetcode day 3

--- leetcode/1-two-sum.py
+++ leetcode/1-two-sum.py
@@ -11,12 +11,39 @@ def twoSumBruteForce(nums, target):
             if nums[i] + nums[j] == target and i != j:
                 return [i, j]
 
+def twoSumEfficientTwoPass(nums, target):
+    """
+    Complement based approach with using extra space 
+    TC:O(n)
+    SC:O(n)
+    what is complement = target - nums[i]
+    e.g let's say target = 9, then perfect complement would be 9 - 7 = 2 and 2 is present in the list 
+    """
+    dict = {}
+    for i in range(len(nums)):
+        dict[nums[i]] = i
+    for i in range(len(nums)):
+        complement = target - nums[i]
+        if complement in dict and dict[complement] != i:
+            return [i, dict[complement]]
+    
 def twoSumEfficient(nums, target):
     """
+    Uses complement based approach but with one single pass
+    TC:O(n)
+    SC:O(n)
     """
-    pass
-
+    dict = {}
+    for i in range(len(nums)):
+        complement = target - nums[i]
+        if(complement in dict):
+            return [i, dict[complement]]
+        dict[nums[i]] = i
+    
 
 
 if __name__ == "__main__":
     print(twoSumBruteForce([2,7,11,15], 9))
+    print(twoSumEfficientTwoPass([2,7,11,15], 9))
+    print(twoSumEfficient([2,7,11,15], 9))
+    
