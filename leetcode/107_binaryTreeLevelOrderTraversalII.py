e4a13c585046fe869890e358ab6fb909466946f7
avishek arora <avishek.arora@icloud.com>
2021-03-08 10:59:46 +0530

    added few ques.

--- leetcode/107_binaryTreeLevelOrderTraversalII.py
+++ leetcode/107_binaryTreeLevelOrderTraversalII.py
@@ -1,11 +1,11 @@
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 class Solution:
-    def levelOrderBottom(self, root: TreeNode) -> List[List[int]]:
+    def levelOrderBottom(self, root: TreeNode) -> [[int]]:
         if not root:
             return []
         q, tq = [root], []
